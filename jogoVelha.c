fa78634b501cdf49688e69bcb09746d754251fe9
Bluebom <franklinhpf@hotmail.com>
2021-12-09 23:17:14 -0300

    update

--- jogoVelha.c
+++ jogoVelha.c
@@ -1,31 +1,16 @@
 #include<stdio.h>;
 #include<stdlib.h>;
 #include<locale.h>;
+#include "likemethods.h";
 
-// variavel global facilita o desenvolvimento do jogo por poder ser acessada facilmente por qualquer função
-char game[3][3]; //Matriz
-char playerOne[50] /* X */,playerTwo[50] /* O */; //Nome dos jogadores
-
-void startMatriz()
-{
-    int i, j;
-    // acessando linhas
-    for(i = 0; i < 3; i ++)
-    {
-        // acessando colunas
-        for(j = 0; j < 3; j++){
-            game[i][j] = 'a';
-        }
-    }
-}
-
-/* SEPARANDO AS LOGICAS EM PEQUENAS FUNÇÃO, FAZENDO COMENTÁRIOS E CRIANDO VARIAVEIS COERENTES QUERO TRAZER O MÁXIMO DE BOAS PRÁTICAS PARA ESSE PROJETO */
+/* SEPARANDO AS LOGICAS EM PEQUENAS FUNÇÕES, FAZENDO COMENTÁRIOS E CRIANDO VARIAVEIS COERENTES QUERO TRAZER O MÁXIMO DE BOAS PRÁTICAS PARA ESSE PROJETO */
 
 // a ideia aqui é verifica se a posição que o usuário escolheu está vazia
 int isVoidPosition(int x, int y){
     if(game[x][y] != 'X' && game[x][y] != 'O'){
         return 1;
     }
+    printf("\n\n Opção inválida! \n\n");
     return 0;
 }
 
@@ -38,70 +23,8 @@ int isValidPosition(int x, int y) {
             return 1;
         }
     }
+    printf("\n\n Opção inválida! \n\n");
     return 0;
-}
-
-// A ideia aqui é validar se o caracter é válido
-int isValidChar(char letra){
-    if(letra == 'X' || letra == 'O')
-        return 1;
-    else
-        return 0;
-}
-
-// a ideia aqui é verificar se a posição das linhas dá a algum dos jogadores a vitória
-int lineWin(){
-    int i, j, same = 1;
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 2; j++){
-            if(isValidChar(game[i][j]) && game[i][j] == game[i][j+1])
-                same++;
-        }
-            if(same == 3)
-                return 1;
-            same = 1;
-    }
-    return 0; // ngm venceu pelas linhas
-}
-
-// a ideia aqui é verificar se a posição das colunas dá a algum dos jogadores a vitória
-int columnWin(){
-    int i, j, same = 1;
-    for(i = 0; i < 2; i++){
-        for(j = 0; j < 3; j ++){
-            if(isValidChar(game[i][j] && game[i][j] == game[i+1][j])) 
-                same++;
-        }
-        if(same == 3)
-            return 1;
-        same = 1;
-    }
-    return 0; // ngm venceu pelas colunas
-}
-
-// a ideia aqui é verificar se a diagonal dá a algum dos jogadores a vitória
-int mainDiagonalWin(){
-    int i, same = 1;
-    for(i = 0; i < 2; i++){
-        if(isValidChar(game[i][i]) && game[i][i] == game[i+1][i+1]){
-            same++;
-        }
-    }
-    if(same == 3)
-        return 1;
-    return 0; //ngm ganha na diagonal
-}
-
-int secondaryDiagonalWin(){
-    int i, j, same = 1;
-    for(i = 0; i < 2; i++){
-        if(isValidChar(game[i][2-i]) && game[i][2-i] == game[i+1][1-i]){
-            same++;
-        }
-    }
-    if(same == 3)
-        return 1;
-    return 0; //ngm ganha na diagonal
 }
 
 void printGame()
